2bee35fd100fd4699d50d5094942f78e845602f6
fenglin <liuqianpan2018@outlook.com>
2021-09-15 17:56:22 +0800

    9.15php镶嵌在html

---
+++ text.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>这是一个案例</h1>
+    <font color="<?php echo 'red' ?>">这 是 一 段 文 字</font><br>
+    <!-- <?php echo '此句话使用 PHP 输出'; ?>-->
+    <?php /* ?><b>此句话被注释。</b><?php */ ?>
+</body>
+</html>
