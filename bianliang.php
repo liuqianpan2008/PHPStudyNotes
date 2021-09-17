9dc4f48646b9fb25df48d65abbb2a57455edf73c
fenglin <liuqianpan2018@outlook.com>
2021-09-17 09:34:30 +0800

    9.17变量与常量

---
+++ bianliang.php
@@ -0,0 +1,17 @@
+<?php
+//定义常量
+    // define("PAI", 3.14);
+    //define("常量名","常量值"，"是否区分大小写'
+    // echo PAI*10*10;
+    // const PAI=3.14;
+    // echo PAI*10*10;
+//系统常量
+//  echo __FILE__;//当前文件目录
+//  echo __LINE__;//当前代码行号
+//  echo PHP_VERSION;//PHP版本
+//  echo PHP_OS;//当前系统
+//变量类型
+    $str1='php';
+    echo "hello $str1";
+    $num = 123;
+    $num2=123.456;
