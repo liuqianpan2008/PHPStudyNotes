5363bf13edfcbcb6de27d06031cc0abd9e9f0be2
fenglin <liuqianpan2018@outlook.com>
2021-09-20 11:33:20 +0800

    9.20作业：换行变色表

---
+++ 换行变色表格.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>表格各行换行</title>
+    <style>
+    </style>
+</head>
+<body>
+    <table border="1" cellspacing="0" cellpadding="5" width="100" style="text-align:center;margin:0 auto">
+        <caption>表格换行</caption>
+        <?php
+            for ($i=0; $i < 10; $i++) {
+                if ($i%2==0) {
+                    echo "<tr style='background: red;'><td>偶数行</td><td>".$i."</td></tr>";
+                } else {
+                    echo "<tr><td>奇数行</td><td>".$i."</td></tr>";
+                }
+            }
+        ?>  
+    </table> 
+</body>
+</html>
